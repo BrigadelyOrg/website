2067b2c5daa1dada669bde59205fe0c776150429
Lawal Selim <lawalselim0@gmail.com>
2026-07-03 11:23:54 +0100

    tag manager

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,12 @@
 import localFont from "next/font/local";
 import { Inter, Libre_Baskerville } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import JsonLd from "@/components/JsonLd";
+
+const GTM_ID = "GTM-NQPV8M79";
 
 const organizationSchema = {
   "@context": "https://schema.org",
@@ -205,6 +208,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en-NG">
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`,
+          }}
+        />
+      </head>
       <body
         className={`
           ${inter.variable}
@@ -223,6 +239,14 @@ export default function RootLayout({ children }) {
           antialiased
         `}
       >
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <JsonLd schema={organizationSchema} />
         <JsonLd schema={websiteSchema} />
         <Header />
